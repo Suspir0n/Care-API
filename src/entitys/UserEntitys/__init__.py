8a22a61ecff6d55a98fbbb1b338bfd334f060d8b
Suspir0n <evandrojunior1615@gmail.com>
2021-01-20 07:03:48 -0300

    Adding a new python file, userEntitys finish

---
+++ src/entitys/UserEntitys/__init__.py
@@ -0,0 +1,31 @@
+from src.entitys.BaseEntitys import BaseEntitys
+from src import init_db
+from sqlalchemy.dialects import *
+
+database = init_db()
+
+
+class UserEntitys(database['SQLAlchemy'].Model):
+    __tablename__ = 'user'
+    firstName = database['SQLAlchemy'].Column(database['SQLAlchemy'].String(100), nullable=False)
+    lastName = database['SQLAlchemy'].Column(database['SQLAlchemy'].String(100), nullable=False)
+    email = database['SQLAlchemy'].Column(database['SQLAlchemy'].String(200), nullable=False)
+    password = database['SQLAlchemy'].Column(database['SQLAlchemy'].String(100), nullable=False)
+    photo = database['SQLAlchemy'].Column(database['SQLAlchemy'].String(200), nullable=False)
+    isRoot = database['SQLAlchemy'].Column(database['SQLAlchemy'].Boolean, default=False)
+
+    def create(self):
+        database['SQLAlchemy'].session.add(self)
+        database['SQLAlchemy'].commit()
+
+
+    def __init__(self, firstName, lastName, email, password, photo, isRoot):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = password
+        self.photo = photo
+        self.isRoot = isRoot
+
+
+database['SQLAlchemy'].create_all()
