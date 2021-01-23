f1ff1283f3a6c21f62a86a4fc2ca8a134d5869e4
Suspir0n <evandrojunior1615@gmail.com>
2021-01-23 09:50:09 -0300

    Implemented the key file for conversion base64

---
+++ src/helpers/UtilHelper/__init__.py
@@ -0,0 +1,11 @@
+from random import random
+import math
+
+
+class UtilsHelper:
+    @staticmethod
+    def generate_unique_hash():
+        def s4():
+            result = str(math.floor((1 + random()) * 0x10000))
+            return result[1:]
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
